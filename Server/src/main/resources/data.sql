417e128ccc985fa1b8e520a1d6d35a7618597d1e
Ian Clement <ian.clement@johnabbott.qc.ca>
2020-12-01 15:20:44 -0500

    Sample data.

--- Server/src/main/resources/data.sql
+++ Server/src/main/resources/data.sql
@@ -1,8 +1,11 @@
-insert into user (email, username, password, uuid) values ('ian@foo.com', 'ian','emacs', '7bdba0fe-fe95-4b1c-8247-f2479ee6e380');
-insert into user (email, username, password, uuid) values ('sandy@foo.com','sandy','vim','2c77dafe-1545-432f-b5b1-3a0011cf7036');
-insert into user (email, username, password, uuid) values ('vik@foo.com','vik','vscode','97489bce-1c85-4ff2-b457-ba53589d12cc');
+insert into user (id, username, password, uuid, islogin) values (1, 'ian','emacs', '7bdba0fe-fe95-4b1c-8247-f2479ee6e380', 0);
+insert into user (id, username, password, uuid, islogin) values (2, 'sandy','vim','2c77dafe-1545-432f-b5b1-3a0011cf7036', 0);
+insert into user (id, username, password, uuid, islogin) values (3, 'vik','vscode','97489bce-1c85-4ff2-b457-ba53589d12cc', 0);
 
-insert into note (uuid, title, body, reminder, created, modified) values ('e187ecea-167c-4feb-a13a-eb34949bb400', 'Foo', 'Foo body.', null, '2018-11-04 12:34:45', '2018-11-05 13:42:44');
+insert into macronutrient (id, protein, fiber, sugar, unsaturatedfat, saturatedfat, transfat, cholesterol, sodium) values (10, 10.0, 1.0, 0.7, 4.0, 0.9, 0.0, 0.0, 0.004);
 
-insert into collaborator (id, user, note) values (1, '7bdba0fe-fe95-4b1c-8247-f2479ee6e380', 'e187ecea-167c-4feb-a13a-eb34949bb400');
+insert into fooditem (id, name, category, servingsize, type, calories, macronutrient) values (100, 'tofu', 'GRAIN', 100, '?', 83, 10);
 
+insert into userdailyconsumption (id, user, date) values (1, 1,  '2020-11-30 13:42:44');
+
+insert into userfooditem (id, fooditem, userdailyconsumption, numberofserving, meal, totalcalories) values (1, 100, 1, 3, 'SUPPER', 249);
