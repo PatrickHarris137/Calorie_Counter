84f1d8f04b46b86abeb1dc3f3c10e91a80c26cdc
Jan Marasigan <1756489@johnabbottcollege.net>
2020-12-02 12:08:10 -0500

    Food Display fragment
    RecyclerVIew with no data, adapter and view holder semi-implemented

---
+++ app/src/main/java/com/example/caloriecounter/FoodDisplay/FoodDisplayViewHolder.java
@@ -0,0 +1,22 @@
+package com.example.caloriecounter.FoodDisplay;
+
+import android.graphics.Color;
+import android.icu.text.SimpleDateFormat;
+import android.text.SpannableString;
+import android.text.style.BackgroundColorSpan;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class FoodDisplayViewHolder  extends RecyclerView.ViewHolder{
+
+
+    public FoodDisplayViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+
+    public void set(Food food){
+
+    }
+}
